f93869825eeca82af17455610509f08485c25d41
thinhbx <thinhbuiunitydev@gmail.com>
2025-07-08 10:14:33 +0700

    Hello, SwiftUI

---
+++ ChatPrototype/ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by ThinhBX on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
